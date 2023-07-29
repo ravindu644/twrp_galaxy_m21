04cb407f20ba4d8277a981939202e49a3b094906
shlokexe <130241775+shlokexe@users.noreply.github.com>
2023-07-29 14:56:24 +0530

    device.mk

--- device.mk
+++ device.mk
@@ -1,8 +1,14 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+#Dynamic Partition Flag
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-LOCAL_PATH := device/samsung/m21
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
+# Apex Libraries
+PRODUCT_HOST_PACKAGES += \
+    libandroidicu
