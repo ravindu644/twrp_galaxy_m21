b8605310add02e97e92e8c10197e72811095100a
shlokexe <130241775+shlokexe@users.noreply.github.com>
2023-07-29 14:55:30 +0530

    BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,14 +1,25 @@
-DEVICE_PATH := device/samsung/m21
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# For building with minimal manifest
-ALLOW_MISSING_DEPENDENCIES := true
+DEVICE_PATH := device/samsung/m21
 
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
+TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a73
+TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
@@ -17,71 +28,103 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Bootloader
+BOARD_VENDOR := samsung
+TARGET_SOC := exynos9611
 TARGET_BOOTLOADER_BOARD_NAME := exynos9611
-BOARD_VENDOR := samsung #value for Vendor
-TARGET_SOC := exynos9611 #value for ro.product.board
-TARGET_BOARD_PLATFORM := universal9611
-TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+TARGET_NO_RADIOIMAGE := true
+
+# Platform
+TARGET_BOARD_PLATFORM := exynos9611
+TARGET_BOARD_PLATFORM_GPU := mali-g72
+PLATFORM_SECURITY_PATCH := 2021-05-01
 
 # Kernel
 BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/11/Image
+#TARGET_KERNEL_SOURCE := kernel/samsung/universal9611
+#TARGET_KERNEL_CONFIG := exynos9610-m21xx_defconfig
 BOARD_KERNEL_CMDLINE := androidboot.hardware=exynos9611
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --header_version 2 --board SRPSG30B004RU
-BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo
-BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/dtb
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
 
-# Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 61865984
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 71106560
-BOARD_DTBOIMG_PARTITION_SIZE := 8388608
+# Filesystem
 BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
-BOARD_SUPER_PARTITION_SIZE := 6382682112
-BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
-BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor product odm
-BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 6382682112
-
-# Recovery
-BOARD_INCLUDE_RECOVERY_DTBO := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-# Verified Boot
-BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 0
-BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 0
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 0
+# Haptics
+TW_USE_SAMSUNG_HAPTICS := true
 
-# Hack: prevent anti rollback
-PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 16.1.0
+# Keymaster Support
+TARGET_KEYMASTER_VARIANT := samsung
 
-# TWRP Configuration
+# MetaData
+BOARD_USES_METADATA_PARTITION := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 61865984
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 71102464
+BOARD_DTBOIMG_PARTITION_SIZE       := 8388608
+BOARD_SUPER_PARTITION_SIZE := 6365921848
+BOARD_SUPER_PARTITION_GROUPS := google_dynamic_partitions
+BOARD_GOOGLE_DYNAMIC_PARTITIONS_PARTITION_LIST := \
+    system \
+    vendor \
+    product \
+    odm
+
+# BOARD_GOOGLE_DYNAMIC_PARTITIONS_SIZE is set to BOARD_SUPER_PARTITION_SIZE / 2 - 4MB
+BOARD_GOOGLE_DYNAMIC_PARTITIONS_SIZE := 3178766620
+
+# Set error limit to BOARD_SUPER_PARTITION_SIZE - 500MB
+BOARD_SUPER_PARTITION_ERROR_LIMIT := 5841633848
+
+# Ramdisk
+BOARD_ROOT_EXTRA_FOLDERS := \
+    prism \
+    optics \
+    metadata \
+    efs \
+    carrier \
+    dqmdbg \
+    spu \
+    keydata \
+    keyrefuge
+
+# Rsync error fix or Fixing trying to copy non-existance files
+TARGET_COPY_OUT_VENDOR := vendor
+
+# TWRP specific build flags
 TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 365
 TW_DEFAULT_BRIGHTNESS := 219
-TW_Y_OFFSET := 84
-TW_H_OFFSET := -84
+TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
+TW_EXTRA_LANGUAGES := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_DEVICE_VERSION := Ravindu Deshan #use your name
-TW_USE_SAMSUNG_HAPTICS := true
-TW_FRAMERATE := 60
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/11/dtbo.img
+BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/11/dtb
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+
+# ANDROID VERIFIED BOOT
+BOARD_AVB_ENABLE := true
+ifeq ($(BOARD_AVB_ENABLE), true)
+BOARD_AVB_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_RECOVERY_KEY_PATH := $(BOARD_AVB_KEY_PATH)
+BOARD_AVB_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_RECOVERY_ALGORITHM := $(BOARD_AVB_ALGORITHM)
+BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := $(BOARD_AVB_ROLLBACK_INDEX)
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 0
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --algorithm NONE
+endif # BOARD_AVB_ENABLE
